c0895294e8652a4a69e479a29e193d598cbafeaa
abraham-ivan <abrahamivan7@gmail.com>
2018-06-17 22:50:53 +0530

    displaymessageactivity completed

--- app/src/main/java/com/example/abraham/testapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/abraham/testapplication/DisplayMessageActivity.java
@@ -1,7 +1,9 @@
 package com.example.abraham.testapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        //get the Intent that started this activity and extract the string from it
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        //capture the layout's TextView and set its text as the message
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+
     }
 }
